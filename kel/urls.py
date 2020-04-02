046c460e45bf20afae44ef163e075db711d024eb
Ebenezer Offei <ebenezeroffei@outlook.com>
2020-04-03 00:31:34 +0100

    Started with the profile section;

--- kel/urls.py
+++ kel/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('landing.urls')),
-    path('register/',include('user.urls')),
+    path('',include('user.urls')),
     path(
         'password-reset/',
         authviews.PasswordResetView.as_view(
--- user/views.py
+++ user/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.views import View
 from django.http import HttpResponseRedirect
 from .models import Location
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from .forms import NewUser,ClientLocation
 
 # Create your views here.
@@ -30,13 +32,20 @@ class NewUserView(View):
             'loc':loc,
         }
         if form.is_valid():
-#            print(form.cleaned_data)
             user = form.save()
             print(user)
             if loc.is_valid():
-#                print(loc.cleaned_data)
                 location = Location(client = user,**loc.cleaned_data)
                 location.save()
             messages.success(request,f'You have been successfully registered, you can now sign in.')
             return HttpResponseRedirect(reverse('user:signin'))
         return render(request,self.template_name,context)
+
+    
+class UserProfileView(View):
+    """ This class displays the profile of the user """
+    template_name = 'user/user_profile.html'
+    
+    @method_decorator(login_required)
+    def dispatch(self,request,*args,**kwargs):
+        return render(request,self.template_name)
