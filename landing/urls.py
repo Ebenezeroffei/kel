465fa5be4298c6fc4ad160c277803e4027e6e6dd
Ebenezer Offei <ebenezeroffei@outlook.com>
2020-04-02 23:25:32 +0100

    Search functionality working;

--- landing/urls.py
+++ landing/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',landing_views.ProductListView.as_view(),name = 'home'),
     path('product/<int:pk>/',landing_views.ProductDetailView.as_view(),name = 'detail'),
     path('add-to-cart/',landing_views.AddItemsToCartView.as_view(),name = 'add_to_cart'),
+    path('search/',landing_views.ItemsSearchView.as_view(),name = 'search'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -30,7 +30,6 @@ class ProductDetailView(generic.DetailView):
         if self.request.user.is_authenticated:
             added_items = [pro for pro in  Product.objects.all() for item in self.request.user.usercartitems_set.all() if pro.product_name == item.item_name ]
             context['added_items'] = added_items
-            print(added_items)
         # Get the product beign showed
         product_id = int(self.kwargs.get('pk'))
         other_products = Product.objects.exclude(id = product_id)
@@ -52,3 +51,17 @@ class AddItemsToCartView(View):
         return JsonResponse(data)
 
     
+class ItemsSearchView(generic.ListView):
+    """ This class returns the search results """
+    model = Product
+    template_name = 'landing/search_results.html'
+    
+    def get_context_data(self,*args,**kwargs):
+        results = self.request.GET.get("q");
+        context = super().get_context_data(*args,**kwargs)
+        context['results'] = Product.objects.filter(product_name__icontains = results)
+        if self.request.user.is_authenticated:
+            added_items = [pro for pro in  Product.objects.all() for item in self.request.user.usercartitems_set.all() if pro.product_name == item.item_name ]
+            context['added_items'] = added_items
+        print(context['results'])
+        return context
